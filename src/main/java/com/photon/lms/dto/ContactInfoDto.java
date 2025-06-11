c3e84add8f1f9922fba96f940c0b8a806aa47042
prabhakarh_p <prabhakar.patil@photon.com>
2025-06-11 20:25:45 +0530

    added spring profiles configuration

---
+++ src/main/java/com/photon/lms/dto/ContactInfoDto.java
@@ -0,0 +1,23 @@
+package com.photon.lms.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Component
+@ConfigurationProperties(prefix = "lms")
+public class ContactInfoDto {
+    private Map<String,String> contactDetails;
+    private List<Long> onCallSupport;
+    private String message;
+
+}
