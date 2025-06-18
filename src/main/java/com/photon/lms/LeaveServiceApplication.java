b7bcab1ceee80bd1f571a1bca0e57291b66fa3be
prabhakarh_p <prabhakar.patil@photon.com>
2025-06-18 15:36:13 +0530

    added logging config and profile config

--- src/main/java/com/photon/lms/LeaveServiceApplication.java
+++ src/main/java/com/photon/lms/LeaveServiceApplication.java
@@ -2,9 +2,10 @@ package com.photon.lms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-
+@EnableScheduling
 public class LeaveServiceApplication {
 
 	public static void main(String[] args) {
